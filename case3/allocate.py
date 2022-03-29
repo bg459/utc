eb17b0a1d5c28a333aa00deb0e4f0d1ee5a6e93b
derekqin8 <derekqin8@gmail.com>
2022-03-28 21:28:24 -0700

    added case 3

---
+++ case3/allocate.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import scipy
+
+#########################################################################
+## Change this code to take in all asset price data and predictions    ##
+## for one day and allocate your portfolio accordingly.                ##
+#########################################################################
+
+def allocate_portfolio(asset_prices, asset_price_predictions_1,
+                       asset_price_predictions_2,
+                       asset_price_predictions_3):
+    # returns a vector (n_assets,) with the weights for the portfolio
+    
+    # This simple strategy equally weights all assets every period
+    # (called a 1/n strategy).
+    
+    n_assets = len(asset_prices)
+    weights = np.repeat(1 / n_assets, n_assets)
+    return weights
